379cc0697a169f36f01d4ef0f61baa083f41adb2
Calum <calumcrawford9@gmail.com>
2024-04-25 18:31:13 -0700

    minor changes

--- app/main.c
+++ app/main.c
@@ -6,27 +6,22 @@
 #define ERROR "ERROR"
 #define RANDOM_FILE "random_file"
 
-void tcp_phase(struct sockaddr_in *sin, unsigned int src_port, unsigned int dst_port, const char* server_ip,
-                     unsigned int ttl, int type) {
-    // type will equal SYN or 
+void tcp_phase(unsigned int port, const char *server_ip, unsigned int ttl) 
+{
+
     int sockfd = init_socket(IPPROTO_TCP);
-
-    char *host = (char*)malloc(NI_MAXHOST);
+    struct hostent *host = gethostbyname(server_ip);
     if (host == NULL) {
-        handle_error(sockfd, "Memory allocation error");
+        handle_error(sockfd, "gethostbyname()");
     }
 
-    get_hostip(host);
-    unsigned long host_addr = inet_addr(host);
-    free(host);
+    in_addr_t server_addr = *(in_addr_t *)host->h_addr_list[0];
 
-    if (host_addr == INADDR_NONE) {
-        handle_error(sockfd, "Invalid address");
-    }
-    unsigned long dst_addr = inet_addr(server_ip);
-    if (dst_addr == INADDR_NONE) {
-        handle_error(sockfd, "Invalid address");
-    }
+    struct sockaddr_in sin;
+    memset(&sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = server_addr;
+    sin.sin_port = htons(port);
     
 
     char buffer[sizeof(struct iphdr) + sizeof(struct tcphdr)];
@@ -41,6 +36,8 @@ void tcp_phase(struct sockaddr_in *sin, unsigned int src_port, unsigned int dst_
                      sizeof(struct tcphdr));
     tcp->check = csum((unsigned short *)buffer, sizeof(struct iphdr) + 
                      sizeof(struct tcphdr));
+    
+    struct sockaddr_in sin;
 
     send_tcp_pckt(buffer, ip->tot_len, sockfd, ip, sin);
 
@@ -66,7 +63,6 @@ void udp_phase(unsigned int udp_dst_port, unsigned int udp_src_port,
                     udp_src_port, server_ip, ttl);
 
     send_udp_pckts(buffer, sockfd, ip, sin, n_pckts, pckt_len, high_entropy);
-
 }
 
 
@@ -96,60 +92,28 @@ int main(int argc, char **argv) {
         printf("ERROR! You must enter a server IP address in the %s file", argv[1]);
         return EXIT_FAILURE;
     }
-    struct hostent *host = gethostbyname(server_ip);
-    if (host == NULL) {
-        perror("Error resolving host");
-        return EXIT_FAILURE;
-    }
-
-    in_addr_t dst_addr = *(in_addr_t *)host->h_addr_list[0];
-
 
     unsigned int hsyn_port = (unsigned int)atoi(get_value(config_file, "TCP_HEADSYN_dest_port_number"));
-    unsigned int udp_dst_port = (unsigned int)atoi(get_value(config_file, "UDP_dest_port_number"));
+    unsigned int tsyn_port = (unsigned int)atoi(get_value(config_file, "TCP_TAILSYN_dest_port_number"));
     unsigned int udp_src_port = (unsigned int)atoi(get_value(config_file, "UDP_src_port_number"));
+    unsigned int udp_dst_port = (unsigned int)atoi(get_value(config_file, "UDP_dest_port_number"));
     unsigned int ttl = (unsigned int)atoi(get_value(config_file, "UDP_packet_TTL"));
-    unsigned int preprob_port = (unsigned int)atoi(get_value(config_file, "TCP_PREPROB_port_number"));
-
-
-
-    if (hsyn_port == 0) {
-        handle_key_error(hsyn_port, "TCP_HEADSYN_dest_port_number", config_file);
-    }
-    if (udp_dst_port == 0) {
-        handle_key_error(udp_dst_port, "UDP_dest_port_number", config_file);
-    }
-
-    if (udp_src_port == 0) {
-        handle_key_error(udp_src_port, "UDP_src_port_number", config_file);
-    }
-
-    if (ttl == 0) {
-        handle_key_error(ttl, "UDP_packet_TTL", config_file);
-    }
-    
-    // src_port, dst_port, server_ip, ttl
-    if (src_port == 0)
-        handle_key_error(src_port, "TCP_PREPROB_port_number", config_file);
-    unsigned int dst_port = (unsigned int)atoi(get_value(config_file, "TCP_HEADSYN_dest_port_number"));
-    if (dst_port == 0)
-        handle_key_error(dst_port, "TCP_HEADSYN_dest_port_number", config_file);
-    
-    struct sockaddr_in sin;
-    memset(&sin, 0, sizeof(sin));
-    sin.sin_family = AF_INET;
-    sin.sin_port = htons(dst_port);
-    sin.sin_addr.s_addr = dst_addr;    // should create the server_addr here pass into tcp_phase
-
-    tcp_phase(&sin, src_port, dst_port, server_ip, ttl, TH_SYN);
-    
     unsigned int m_time = (unsigned int)atoi(get_value(config_file, "measurement_time"));
+
+    if (hsyn_port == 0) 
+        handle_key_error(hsyn_port, "TCP_HEADSYN_dest_port_number", config_file);
+    if (tsyn_port == 0)
+        handle_key_error(tsyn_port, "TCP_TAILSYN_dest_port_number", config_file);
+    if (udp_src_port == 0) 
+        handle_key_error(udp_dst_port, "UDP_src_port_number", config_file);
+    if (udp_dst_port == 0) 
+        handle_key_error(udp_src_port, "UDP_dest_port_number", config_file);    
+    if (ttl == 0) 
+        handle_key_error(ttl, "UDP_packet_TTL", config_file);
     if (m_time == 0)
         handle_key_error(m_time, "measurement_time", config_file);
-     // server
 
-    
-
+    tcp_phase(src_port, dst_port, server_ip, ttl);
+        
     return EXIT_SUCCESS;
-
 }
