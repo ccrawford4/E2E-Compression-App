3535fb587871c34c66e1910e6c020c72492ce8f4
ccrawford4 <calumcrawford9@gmail.com>
2024-05-05 09:34:54 -0700

    Updated header file

--- app/headers/json.h
+++ app/headers/json.h
@@ -1,6 +1,6 @@
 #include <jansson.h>
 #define NUM_ITEMS 11
 
-void handle_key_error(int ret_val, char* key, char* file_name);
+void handle_key_error(char* key, char* file_name);
 const char* get_default(char* key);
 const char* get_value(char* file_path, char* key);
